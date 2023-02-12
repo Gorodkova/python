94807f98447f21b0bcf33123fab6adeebf902799
Gorodkova <125132548+Gorodkova@users.noreply.github.com>
2023-02-12 22:24:40 +0700

    Add files via upload

--- 3 lesson/first.py
+++ 3 lesson/first.py
@@ -1,11 +1,9 @@
 a=int(input())
-
-if a==5:
-    print(5)
-elif a>=10:
-    print(">10")
-elif a<=3:
-    print("<3")
+if a>100:
+    print("Число больше 100")
+elif a<100 and a>50:
+    print("Число между 50 и 100")
+elif a<50:
+    print("Число меньше 50")
 else:
     print("Условие не верное")
-
